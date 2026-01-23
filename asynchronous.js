65448de7d7d9431a4cdd92521c609a485eabb81e
viveklohar229 <vishwakarmavivek521@gmail.com>
2026-01-23 12:16:23 +0530

    Handle promises and async and await

--- asynchronous.js
+++ asynchronous.js
@@ -16,3 +16,32 @@
 // },2000);
 
 // console.log(first + " " + last);
+
+
+// Resolve this drawback from using Promises
+
+let first = "Pullu";
+let last = "";
+
+const setLastName = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // last = "Vivek";
+        resolve("Vivek");
+    }, 2000);
+
+})
+
+// setLastName.then((data) => {
+//     last = data;
+//     // console.log(first + " " + last); 
+//     console.log(first + " " + data);
+// })
+
+// console.log("Hello")
+
+const displayLastName = async () => {
+    last = await setLastName;
+    console.log(`${first} ${last}`);
+}
+
+displayLastName();
