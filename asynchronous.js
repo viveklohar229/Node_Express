59d4e8219bf2ce548d98563eace8c78e04ce0d30
viveklohar229 <vishwakarmavivek521@gmail.com>
2026-01-23 10:59:01 +0530

    asynchronous

---
+++ asynchronous.js
@@ -0,0 +1,18 @@
+// console.log("a")
+// // console.log("b")
+// setTimeout(() => {
+//     console.log("b")
+// })
+// console.log("c")
+
+
+// Drawback of asynchronous
+
+// let first = "Vini";
+// let last = "";
+
+// setTimeout(()  => {
+//      last =  "Vivek";
+// },2000);
+
+// console.log(first + " " + last);
